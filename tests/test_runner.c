f26548980a20a0708699cc26afd886f40296394a
max <maxr777@proton.me>
2026-06-04 04:13:39 +0200

    Remove stdout buffering in test_runner.c for cleaner output in CI

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -35,13 +35,15 @@ static u64 now_ns(void) {
 #include "game_load_test.c"
 
 int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+
 	u64 start = now_ns();
 
-	puts("Test: correct game load\n");
+	puts("Test: correct game load");
 	test_game_load();
-	puts("Test: game missing when loading\n");
+	puts("Test: game missing when loading");
 	test_game_load_missing();
-	puts("Test: game too small to load\n");
+	puts("Test: game too small to load");
 	test_game_load_too_small();
 
 	u64 end = now_ns();
