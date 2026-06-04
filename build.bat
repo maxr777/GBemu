6cb959a8e8a685a93462f34cb300f8b0b3781082
max <maxr777@proton.me>
2026-06-04 04:05:55 +0200

    Remove -Wall from raylib's compilation for cleaner output

--- build.bat
+++ build.bat
@@ -11,28 +11,28 @@ if not exist "%RAYLIB_LIB%" (
         -I"%RAYLIB%\external\glfw\include" ^
         -DPLATFORM_DESKTOP ^
         -DGRAPHICS_API_OPENGL_33 ^
-        -std=c11 -Wall
+        -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
-    gcc -c "%RAYLIB%\rshapes.c" -o "%RAYLIB%\rshapes_win.obj" -O2 -I"%RAYLIB%" -std=c11 -Wall
+    gcc -c "%RAYLIB%\rshapes.c" -o "%RAYLIB%\rshapes_win.obj" -O2 -I"%RAYLIB%" -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
-    gcc -c "%RAYLIB%\rtextures.c" -o "%RAYLIB%\rtextures_win.obj" -O2 -I"%RAYLIB%" -std=c11 -Wall
+    gcc -c "%RAYLIB%\rtextures.c" -o "%RAYLIB%\rtextures_win.obj" -O2 -I"%RAYLIB%" -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
-    gcc -c "%RAYLIB%\rtext.c" -o "%RAYLIB%\rtext_win.obj" -O2 -I"%RAYLIB%" -std=c11 -Wall
+    gcc -c "%RAYLIB%\rtext.c" -o "%RAYLIB%\rtext_win.obj" -O2 -I"%RAYLIB%" -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
-    gcc -c "%RAYLIB%\rmodels.c" -o "%RAYLIB%\rmodels_win.obj" -O2 -I"%RAYLIB%" -std=c11 -Wall
+    gcc -c "%RAYLIB%\rmodels.c" -o "%RAYLIB%\rmodels_win.obj" -O2 -I"%RAYLIB%" -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
-    gcc -c "%RAYLIB%\utils.c" -o "%RAYLIB%\utils_win.obj" -O2 -I"%RAYLIB%" -std=c11 -Wall
+    gcc -c "%RAYLIB%\utils.c" -o "%RAYLIB%\utils_win.obj" -O2 -I"%RAYLIB%" -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
     gcc -c "%RAYLIB%\rglfw.c" -o "%RAYLIB%\rglfw_win.obj" -O2 ^
         -I"%RAYLIB%" ^
         -I"%RAYLIB%\external\glfw\include" ^
-        -std=c11 -Wall
+        -std=c11
     if errorlevel 1 exit /b %errorlevel%
 
     ar rcs "%RAYLIB_LIB%" %RAYLIB_OBJECTS%
--- build.sh
+++ build.sh
@@ -20,18 +20,18 @@ if [ ! -f "$RAYLIB_LIB" ]; then
         -DPLATFORM_DESKTOP \
         -DGRAPHICS_API_OPENGL_33 \
         -D_GLFW_X11 \
-        -std=c11 -Wall
+        -std=c11
 
-    gcc -c "$RAYLIB/rshapes.c" -o "$RAYLIB/rshapes.o" -O2 -I"$RAYLIB" -std=c11 -Wall
-    gcc -c "$RAYLIB/rtextures.c" -o "$RAYLIB/rtextures.o" -O2 -I"$RAYLIB" -std=c11 -Wall
-    gcc -c "$RAYLIB/rtext.c" -o "$RAYLIB/rtext.o" -O2 -I"$RAYLIB" -std=c11 -Wall
-    gcc -c "$RAYLIB/rmodels.c" -o "$RAYLIB/rmodels.o" -O2 -I"$RAYLIB" -std=c11 -Wall
-    gcc -c "$RAYLIB/utils.c" -o "$RAYLIB/utils.o" -O2 -I"$RAYLIB" -std=c11 -Wall
+    gcc -c "$RAYLIB/rshapes.c" -o "$RAYLIB/rshapes.o" -O2 -I"$RAYLIB" -std=c11
+    gcc -c "$RAYLIB/rtextures.c" -o "$RAYLIB/rtextures.o" -O2 -I"$RAYLIB" -std=c11
+    gcc -c "$RAYLIB/rtext.c" -o "$RAYLIB/rtext.o" -O2 -I"$RAYLIB" -std=c11
+    gcc -c "$RAYLIB/rmodels.c" -o "$RAYLIB/rmodels.o" -O2 -I"$RAYLIB" -std=c11
+    gcc -c "$RAYLIB/utils.c" -o "$RAYLIB/utils.o" -O2 -I"$RAYLIB" -std=c11
     gcc -c "$RAYLIB/rglfw.c" -o "$RAYLIB/rglfw.o" -O2 \
         -I"$RAYLIB" \
         -I"$RAYLIB/external/glfw/include" \
         -D_GLFW_X11 \
-        -std=c11 -Wall
+        -std=c11
 
     ar rcs "$RAYLIB_LIB" "${RAYLIB_OBJECTS[@]}"
 fi
